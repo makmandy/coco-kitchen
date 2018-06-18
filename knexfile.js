465ae8d199962c9bdbc3633a5f62ac7ac895f8b5
Mandy Mak <mandymak22@gmail.com>
2018-06-18 00:57:54 -0400

    Update knexfile

--- knexfile.js
+++ knexfile.js
@@ -7,12 +7,4 @@ module.exports = {
       database: 'cocokitchen',
     },
   },
-  production: {
-    client: 'mysql',
-    connection: {
-      host: DB_HOST,
-      user: DB_USER,
-      password: DB_PASSWORD,
-    },
-  },
 };
